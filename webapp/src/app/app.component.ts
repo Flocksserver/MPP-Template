1a9e735d672b7dba8e53e732c10d459c7ec0a1fd
Marcel Kaufmann <>
2018-11-01 14:57:42 +0100

    js changes

---
+++ webapp/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import * as shared from '../../../shared-mp/build/classes/kotlin/js/main/shared-mp';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'webapp';
+
+}
